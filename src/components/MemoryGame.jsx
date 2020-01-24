16548923bd97efc5760dd98b478943608a64ba84
Belov Roman <belov.rom-77@yandex.ru>
2020-01-24 08:08:02 +0300

    initial commit

---
+++ src/components/MemoryGame.jsx
@@ -0,0 +1,206 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRedo, faTrophy } from '@fortawesome/free-solid-svg-icons';
+import {
+  openCard,
+  matchCards,
+  lockCards,
+  unLockCards,
+  addSecond,
+  startNewGame,
+} from '../redux/actions/actions';
+import findMinutes from '../functions/findMinutes';
+import findSeconds from '../functions/findSeconds';
+import convertNumToStars from '../functions/convertNumToStars';
+
+let timerId;
+
+const Title = styled.h1`
+  margin-bottom: 7px;
+`;
+
+const GameStatus = styled.section`
+  display: flex;
+  align-items: center;
+  p {
+    margin-bottom: 0;
+    font-size: 21px;
+  }
+  & > * {
+    margin-right: 50px;
+  }
+  & > *:last-child {
+    margin-right: 0px;
+  }
+`;
+
+const Desk = styled.section`
+  margin-top: 10px;
+  margin-bottom: 10px;
+  width: 70vw;
+  height: 70vw;
+  max-width: 660px;
+  max-height: 660px;
+  background: #283c86; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to top,
+    #283c86,
+    #45a247
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to top, #283c86, #45a247);
+  padding: 3%;
+  border-radius: 10px;
+  box-shadow: 12px 15px 20px 0 rgba(46, 61, 73, 0.5);
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Card = styled.div`
+  height: 21%;
+  width: 21%;
+  background: ${props => (props.open ? '#02b3e4' : '#2e3d49')};
+  font-size: ${props => (props.open ? '43px' : '0px')};
+  color: #ffffff;
+  border-radius: 8px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 5px 2px 20px 0 rgba(46, 61, 73, 0.5);
+`;
+
+const WinnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  & > div {
+    font-size: 150px;
+    color: #02ccba;
+  }
+  & > h1 {
+    font-weight: normal;
+  }
+  & > button {
+    background-color: #02ccba;
+    border: 1px solid #02b3e4;
+    border-radius: 10px;
+    font-size: 32px;
+    padding: 10px;
+  }
+`;
+
+function MemoryGame({
+  cards,
+  onOpenCard,
+  openCards,
+  isLocked,
+  cardsLeft,
+  moves,
+  onStart,
+  timer,
+  stars,
+}) {
+  useEffect(() => onStart(), []);
+  useEffect(() => {
+    console.log('Clearing Interval!');
+    if (!cardsLeft) {
+      clearInterval(timerId);
+    }
+  }, [cardsLeft]);
+  const field = (
+    <>
+      <Title>Memory Game</Title>
+      <GameStatus>
+        <div>
+          {convertNumToStars(stars).map(el => (
+            <FontAwesomeIcon icon={el} />
+          ))}
+        </div>
+        <p>{`${findMinutes(timer).edited} : ${findSeconds(timer).edited}`}</p>
+        <p>{`${moves} Moves`}</p>
+        <FontAwesomeIcon onClick={onStart} icon={faRedo} />
+      </GameStatus>
+      <Desk>
+        {cards.map((el, index) => (
+          <Card
+            onClick={
+              el.open || isLocked ? null : () => onOpenCard(index, openCards)
+            }
+            key={index}
+            open={el.open}
+          >
+            <FontAwesomeIcon icon={el.icon} />
+          </Card>
+        ))}
+      </Desk>
+    </>
+  );
+
+  const gameWinStats = (
+    <WinnerContainer>
+      <div>
+        <FontAwesomeIcon icon={faTrophy} />
+      </div>
+      <h1>You are a Winner!!!</h1>
+      <p>
+        {`You won in ${findMinutes(timer).usual} minutes, ${
+          findSeconds(timer).usual
+        } seconds, using ${moves} moves, for ${stars} stars`}
+      </p>
+      <button onClick={onStart} type="button">
+        Start another game!
+      </button>
+    </WinnerContainer>
+  );
+  return <>{cardsLeft ? field : gameWinStats}</>;
+}
+
+// ExampleComponent.propTypes = {
+//   property1: PropTypes.arrayOf(PropTypes.object).isRequired,
+//   property2: PropTypes.number.isRequired,
+//   property3: PropTypes.func.isRequired,
+// }
+
+function mapStateToProps(state) {
+  return {
+    cards: state.appState.cards,
+    openCards: state.appState.openCards,
+    isLocked: state.appState.isLocked,
+    cardsLeft: state.appState.cardsLeft,
+    moves: state.appState.moves,
+    timer: state.appState.timer,
+    stars: state.appState.stars,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onOpenCard: (index, openCards) => {
+      if (openCards.length === 1) {
+        dispatch(lockCards());
+        dispatch(openCard(index));
+        setTimeout(() => {
+          dispatch(matchCards());
+          dispatch(unLockCards());
+        }, 500);
+        return;
+      }
+      dispatch(openCard(index));
+    },
+    onStart: () => {
+      clearInterval(timerId);
+      timerId = setInterval(() => {
+        dispatch(addSecond());
+      }, 1000);
+      dispatch(startNewGame());
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MemoryGame);
